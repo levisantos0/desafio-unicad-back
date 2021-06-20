4318977de697c5f12bc8c27fda860e165c808a80
Levi Barbosa <levisantos2106@gmail.com>
2021-06-20 05:55:13 -0300

    Alteração com configuração do mongoose.

--- app.js
+++ app.js
@@ -2,24 +2,11 @@
 const express = require('express')
 const cors = require('cors')
 const app =  express()
-const mongoose = require('mongoose')
+const mongoose = require('./config/mongoose')
 const PORT = 8081
 
 app.use(cors());
 app.use(express.json());
-
-
-mongoose.Promise = global.Promise;
-
-mongoose.connect("mongodb://localhost/cadastro_entregas",{
-    useUnifiedTopology:true,
-    useNewUrlParser:true,
-    useFindAndModify:false
-}).then(()=>{
-    console.log(`Conectado ao mongodb`)
-}).catch((erro)=>{
-    console.log(`Erro ao se conectar: ${erro}`)
-})
 
 const entregas = require("./routes/entregas")
 app.use(entregas)
